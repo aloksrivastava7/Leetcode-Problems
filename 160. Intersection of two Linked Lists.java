2fe1754accc8174618fef74efdfaefb936822c01
Alok Srivastava <awwwssmme@gmail.com>
2021-08-25 22:20:08 +0530

    Create 160. Intersection of two Linked Lists.java

---
+++ 160. Intersection of two Linked Lists.java
@@ -0,0 +1,75 @@
+/**
+Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+
+The test cases are generated such that there are no cycles anywhere in the entire linked structure.
+
+Note that the linked lists must retain their original structure after the function returns.
+
+Custom Judge:
+
+The inputs to the judge are given as follows (your program is not given these inputs):
+
+intersectVal - The value of the node where the intersection occurs. This is 0 if there is no intersected node.
+listA - The first linked list.
+listB - The second linked list.
+skipA - The number of nodes to skip ahead in listA (starting from the head) to get to the intersected node.
+skipB - The number of nodes to skip ahead in listB (starting from the head) to get to the intersected node.
+The judge will then create the linked structure based on these inputs and pass the two heads, headA and headB to your program. If you correctly return the intersected node, then your solution will be accepted.
+
+Example 1:
+Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+Output: Intersected at '8'
+Explanation: The intersected node's value is 8 (note that this must not be 0 if the two lists intersect).
+From the head of A, it reads as [4,1,8,4,5]. From the head of B, it reads as [5,6,1,8,4,5]. There are 2 nodes before the intersected node in A; There are 3 nodes before the intersected node in B.
+
+Example 2:
+Input: intersectVal = 2, listA = [1,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+Output: Intersected at '2'
+Explanation: The intersected node's value is 2 (note that this must not be 0 if the two lists intersect).
+From the head of A, it reads as [1,9,1,2,4]. From the head of B, it reads as [3,2,4]. There are 3 nodes before the intersected node in A; There are 1 node before the intersected node in B.
+
+Example 3:
+Input: intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+Output: No intersection
+Explanation: From the head of A, it reads as [2,6,4]. From the head of B, it reads as [1,5]. Since the two lists do not intersect, intersectVal must be 0, while skipA and skipB can be arbitrary values.
+Explanation: The two lists do not intersect, so return null.
+**/
+
+#Solution
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null)
+            return null;
+        ListNode tempA = headA;
+        ListNode tempB = headB;
+        while(tempA != tempB) {
+            tempA = tempA == null ? headB : tempA.next;
+            tempB = tempB == null ? headA : tempB.next;
+        }
+        return tempA;
+    }
+}
+
+#Approach
+
+/**
+* We'll traverse through both the linked lists simultaneously. For that we'll maintain two dummy pointers. We'll keep traversing untill the references of both the dummy pointers are same doesn't matter whethere they point to null or point to any specific node. If they'll point to a specific node, then that's the point of intersection b/w both the linkedlist. If there is any point of intersection b/w both the linkedlists, then both the references will finally point to null and we'll return it.
+* While traversing if any of the linkedlist reaches its end ( which shows that it is smaller in length as compared to other ), then we'll give it the reference to the first node of the other linkedlist in order to cover that extra length gap and then at one point of time, both of references will be traversing with similar positions.
+*  ```
+	 tempA = tempA == null ? headB : tempA.next;
+	 tempB = tempB == null ? headA : tempB.next;
+	 ```
+		These two conditions will do as explained in point 2.
+**/
